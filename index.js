5ba6d8a19ede397333927db74d2773c0efc57037
Piotr Krzaczkowski <slimshade93@wp.pl>
2021-03-04 22:48:04 +0100

    js logic implementations

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const celcius = document.getElementById("celciu");
+const celcius = document.getElementById("celcius");
 const fahrenheit = document.getElementById("fahrenheit");
 const kelvin = document.getElementById("kelvin");
 
@@ -6,15 +6,18 @@ const inputs = [...document.getElementsByClassName("input")];
 
 inputs.forEach((input) =>
   input.addEventListener("input", function (e) {
-    let value = parseFloat(e.target.value);
+    let value = e.target.value;
 
     if (e.target.name === "celcius") {
       fahrenheit.value = value * 1.8 + 32;
       kelvin.value = value + 273.15;
     } else if (e.target.name === "fahrenheit") {
-      console.log("fahrrr");
+      console.log("fff");
+      celcius.value = (value - 32) / 1.8;
+      kelvin.value = (value - 32) / 1.8 + 293.15;
     } else if (e.target.name === "kelvin") {
-      console.log("kellllvin");
+      celcius.value = value - 273.15;
+      fahrenheit.value = (value - 273.15) * 1.8 + 32;
     }
   })
 );
